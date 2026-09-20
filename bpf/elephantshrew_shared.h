63412f3e72810a11d569b8079df3e22f83451b52
Roy Cohen <roy@0xbadcaffe.dev>
2026-09-20 15:10:09 +0300

    epbf(ABI): fixed files indent

--- bpf/elephantshrew_shared.h
+++ bpf/elephantshrew_shared.h
@@ -12,25 +12,54 @@
 #define ES_TC_DROP 2
 #define ES_TC_REDIRECT 7
 
-enum es_direction { ES_INGRESS = 0, ES_EGRESS = 1, ES_DIRECTIONS = 2 };
-enum es_hook { ES_HOOK_TC = 1, ES_HOOK_XDP = 2 };
+enum es_direction {
+    ES_INGRESS = 0,
+    ES_EGRESS = 1,
+    ES_DIRECTIONS = 2
+};
+
+enum es_hook {
+    ES_HOOK_TC = 1,
+    ES_HOOK_XDP = 2
+};
+
 enum es_parse_status {
-    ES_PARSE_OK = 0, ES_PARSE_TRUNCATED = 1, ES_PARSE_UNSUPPORTED = 2,
-    ES_PARSE_MALFORMED = 3, ES_PARSE_FRAGMENT = 4, ES_PARSE_SKIPPED = 5
+    ES_PARSE_OK = 0,
+    ES_PARSE_TRUNCATED = 1,
+    ES_PARSE_UNSUPPORTED = 2,
+    ES_PARSE_MALFORMED = 3,
+    ES_PARSE_FRAGMENT = 4,
+    ES_PARSE_SKIPPED = 5
 };
+
 enum es_tuple_flags {
-    ES_T_L2_VALID = 1u << 0, ES_T_ADDR_VALID = 1u << 1,
-    ES_T_PORTS_VALID = 1u << 2, ES_T_FRAGMENT = 1u << 3
+    ES_T_L2_VALID = 1u << 0,
+    ES_T_ADDR_VALID = 1u << 1,
+    ES_T_PORTS_VALID = 1u << 2,
+    ES_T_FRAGMENT = 1u << 3
 };
+
 enum es_event_flags {
-    ES_E_PREFIX_TRUNCATED = 1u << 0, ES_E_COPY_FAILED = 1u << 1,
+    ES_E_PREFIX_TRUNCATED = 1u << 0,
+    ES_E_COPY_FAILED = 1u << 1,
     ES_E_VLAN_STRIPPED = 1u << 2
 };
+
 enum es_counter_id {
-    ES_C_PACKETS, ES_C_BYTES, ES_C_SELECTED, ES_C_EVENTS, ES_C_RING_LOST,
-    ES_C_PARSE_PARTIAL, ES_C_PARSE_MALFORMED, ES_C_SNAPSHOT_FAILED,
-    ES_C_FLOW_UPDATE_FAILED, ES_C_POLICY_DROP, ES_C_MIRROR_ATTEMPT,
-    ES_C_MIRROR_ERROR, ES_C_REDIRECT_ATTEMPT, ES_C_MAX
+    ES_C_PACKETS,
+    ES_C_BYTES,
+    ES_C_SELECTED,
+    ES_C_EVENTS,
+    ES_C_RING_LOST,
+    ES_C_PARSE_PARTIAL,
+    ES_C_PARSE_MALFORMED,
+    ES_C_SNAPSHOT_FAILED,
+    ES_C_FLOW_UPDATE_FAILED,
+    ES_C_POLICY_DROP,
+    ES_C_MIRROR_ATTEMPT,
+    ES_C_MIRROR_ERROR,
+    ES_C_REDIRECT_ATTEMPT,
+    ES_C_MAX
 };
 
 /* Address bytes and ports retain network order. Other integers use host order. */
